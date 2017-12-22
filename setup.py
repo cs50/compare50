7eee3f7a985897ddadd22b14f65ee46862265cdd
David J. Malan <malan@harvard.edu>
2017-12-21 22:31:17 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    author="CS50",
+    author_email="sysadmins@cs50.harvard.edu",
+    classifiers=[
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3",
+        "Topic :: Education",
+        "Topic :: Utilities"
+    ],
+    description="This is compare50, with which you can compare files for similarities.",
+    install_requires=["backports.shutil_get_terminal_size"],
+    keywords=["compare", "compare50"],
+    name="compare50",
+    scripts=["compare50"],
+    url="https://github.com/cs50/compare50",
+    version="1.0.0"
+)
