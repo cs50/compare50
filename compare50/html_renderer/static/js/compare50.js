16736195c0cb2f81187c4c00a0d882c957e6b874
jelleas <jelle.van.assema@gmail.com>
2018-07-26 17:04:00 -0400

    create reverse mapping in .js

---
+++ compare50/html_renderer/static/js/compare50.js
@@ -0,0 +1,27 @@
+var span_to_group = null;
+var fragment_to_spans = null;
+
+function init_maps() {
+  // Create span_to_group
+  span_to_group = new Array(span_to_fragments.length);
+  group_to_spans.forEach((spans, group) => {
+    spans.forEach(span => span_to_group[span] = group);
+  });
+
+  // Create fragment_to_spans
+  fragment_to_spans = {};
+  let fragments = new Set();
+  span_to_fragments.forEach(frags => frags.forEach(frag => fragments.add(frag)));
+  fragments.forEach((frag) => fragment_to_spans[frag] = []);
+  span_to_fragments.forEach((frags, span) => {
+    frags.forEach(frag => fragment_to_spans[frag].push(span));
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  init_maps();
+  console.log("group_to_spans", group_to_spans);
+  console.log("span_to_fragments", span_to_fragments);
+  console.log("span_to_group", span_to_group);
+  console.log("fragment_to_spans", fragment_to_spans);
+});
