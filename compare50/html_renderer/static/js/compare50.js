dc5d7f0d9ba554a7cbb1cfa945c5f34574000bc2
jelleas <jelle.van.assema@gmail.com>
2018-07-26 17:34:25 -0400

    maps on objects intead of arrays

--- compare50/html_renderer/static/js/compare50.js
+++ compare50/html_renderer/static/js/compare50.js
@@ -3,15 +3,17 @@ var fragment_to_spans = null;
 
 function init_maps() {
   // Create span_to_group
-  span_to_group = new Array(span_to_fragments.length);
-  group_to_spans.forEach((spans, group) => {
+  span_to_group = {};
+  Object.keys(group_to_spans).forEach(group => {
+    let spans = group_to_spans[group];
     spans.forEach(span => span_to_group[span] = group);
   });
 
   // Create fragment_to_spans
   fragment_to_spans = {};
-  span_to_fragments.forEach(frags => frags.forEach(frag => fragment_to_spans[frag] = []));
-  span_to_fragments.forEach((frags, span) => {
+  Object.values(span_to_fragments).forEach(frags => frags.forEach(frag => fragment_to_spans[frag] = []));
+  Object.keys(span_to_fragments).forEach(span => {
+    let frags = span_to_fragments[span];
     frags.forEach(frag => fragment_to_spans[frag].push(span));
   });
 }
