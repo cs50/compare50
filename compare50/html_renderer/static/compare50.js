f4f977253a27c4a15a1c728f435b39d3a70c0350
jelleas <jelle.van.assema@gmail.com>
2018-07-27 12:43:49 -0400

    Objects in .js

--- compare50/html_renderer/static/compare50.js
+++ compare50/html_renderer/static/compare50.js
@@ -1,6 +1,62 @@
 var span_to_group = null;
 var fragment_to_spans = null;
 
+function Group(id) {
+  this.id = id;
+  this.spans = [];
+  return this;
+}
+Group.prototype.init = function(fragments, spans, groups) {
+  this.spans = [];
+  let obj = this;
+  group_to_spans[this.id].forEach(span => obj.spans.push(spans[span]));
+}
+
+function Span(id) {
+  this.id = id;
+  this.fragments = [];
+  this.group = null;
+  return this;
+}
+Span.prototype.init = function(fragments, spans, groups) {
+  this.fragments = [];
+  let obj = this;
+  span_to_fragments[this.id].forEach(frag_id => obj.fragments.push(fragments[frag_id]));
+  this.group = groups[span_to_group[this.id]];
+}
+
+function Fragment(id) {
+  this.id = id;
+  this.dom_element = document.getElementById(id);
+  this.fragments = [];
+  this.span = null;
+  this.group = null;
+  this.spans = [];
+  return this;
+}
+Fragment.prototype.init = function(fragments, spans, groups) {
+  if (fragment_to_spans[this.id] !== undefined) {
+    this.spans = [];
+    this.groups = [];
+    for (span_id of fragment_to_spans[this.id]) {
+      this.spans.push(spans[span_id]);
+      this.groups.push(groups[span_to_group[span_id]]);
+    }
+
+    // Get closest enclosing span & group
+    this.span = this.spans[0];
+    this.group = this.groups[0];
+
+    this.fragments = [];
+    for (span of this.spans) {
+      for (frag_id of span_to_fragments[span.id]) {
+        this.fragments.push(fragments[frag_id]);
+      }
+    }
+  }
+}
+
+
 function init_maps() {
   // Create span_to_group
   span_to_group = {};
@@ -42,20 +98,30 @@ function add_mouse_over_listeners() {
   }
 }
 
-function get_grouped_fragments(fragment) {
-  if (fragment_to_spans[fragment] === undefined) {
-    return [];
+function init_objects() {
+  let frags = document.getElementsByClassName("fragment");
+  let fragments = {};
+  let spans = {};
+  let groups = {};
+
+  for (frag of frags) {
+    fragments[frag.id] = new Fragment(frag.id);
   }
 
-  // Get closest enclosing span
-  let span = fragment_to_spans[fragment][0];
+  for (span_id of Object.keys(span_to_group)) {
+    spans[span_id] = new Span(span_id);
+  }
 
-  // Get all fragments that need to be highlighted
-  let group = span_to_group[span];
-  let spans = group_to_spans[group];
-  let grouped_frags = [];
-  spans.forEach(s => span_to_fragments[s].forEach(f => grouped_frags.push(f)));
-  return grouped_frags;
+  for (group_id of Object.keys(group_to_spans)) {
+    groups[group_id] = new Group(group_id);
+  }
+
+  console.log(fragments);
+  Object.values(fragments).forEach(frag => frag.init(fragments, spans, groups));
+  Object.values(spans).forEach(span => span.init(fragments, spans, groups));
+  Object.values(groups).forEach(group => group.init(fragments, spans, groups));
+
+  return [fragments, spans, groups];
 }
 
 document.addEventListener("DOMContentLoaded", function(event) {
@@ -65,5 +131,8 @@ document.addEventListener("DOMContentLoaded", function(event) {
   console.log("span_to_group", span_to_group);
   console.log("fragment_to_spans", fragment_to_spans);
 
-  add_mouse_over_listeners();
+  objs = init_objects();
+  console.log(objs);
+
+  //add_mouse_over_listeners();
 });
