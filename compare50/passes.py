3eb4cd9223be236b0924f5ed407a0a10eba0cac1
Chad Sharp <chad@cs50.harvard.edu>
2018-08-22 15:36:01 -0400

    rename winnowing pass

--- compare50/passes.py
+++ compare50/passes.py
@@ -6,8 +6,8 @@ from ._data import Pass
 __all__ = ["winnowing", "winnowing_exact", "misspellings"]
 
 
-class winnowing(Pass):
-    """Removes all whitespace, normalizes all comments, variable names, and string literals, then uses the winnowing algorithm to compare submissions. """
+class structure(Pass):
+    """Compares code structure by removing all whitespace, normalizing all comments, variable names, and string literals, and running the winnowing algorithm."""
 
     preprocessors = [preprocessors.strip_whitespace,
                      preprocessors.strip_comments,
