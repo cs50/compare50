ec7aa015b8c11b21571143187e1db633c40a09a7
Chad Sharp <chad@cs50.harvard.edu>
2019-06-24 16:58:29 -0400

    Revert "don't count matches that appear in at least 1/10th of the submissions"
    This reverts commit 78e363e3d28b5526c60bfc0664e43f712ca92a99.

--- compare50/comparators/_winnowing.py
+++ compare50/comparators/_winnowing.py
@@ -62,7 +62,7 @@ class Winnowing(Comparator):
         archive_index.include_all(submission_index)
 
         N = len(submissions) + len(archive_submissions)
-        return submission_index.compare(archive_index, score=lambda h: 0 if frequency_map[h] > N//10 else math.log(N / (1 + frequency_map[h])))
+        return submission_index.compare(archive_index, score=lambda h: math.log(N / (1 + frequency_map[h])))
 
     def compare(self, scores, ignored_files):
 
