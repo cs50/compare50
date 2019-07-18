4143aea126c25273c55e6f2470c4be7c2653a2e0
Chad Sharp <chad@cs50.harvard.edu>
2019-07-18 11:56:03 -0400

    version++

--- setup.py
+++ setup.py
@@ -21,6 +21,6 @@ setup(
     packages=find_packages(exclude=["tests"]),
     scripts=["bin/compare50"],
     url="https://github.com/cs50/compare50",
-    version="1.0.3",
+    version="1.1.0",
     include_package_data=True,
 )
