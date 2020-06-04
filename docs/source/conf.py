e708c7b10c2f9f2228660abd126a540298fea1e1
jelleas <jelle.van.assema@gmail.com>
2020-06-04 15:56:07 +0200

    include __init__ in autodoc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,6 +7,16 @@ _tool = "compare50"
 # Add path to module for autodoc
 sys.path.insert(0, os.path.abspath(f'../../{_tool}'))
 
+# Include __init__ in autodoc
+# Source: https://stackoverflow.com/questions/5599254/how-to-use-sphinxs-autodoc-to-document-a-classs-init-self-method
+def skip(app, what, name, obj, would_skip, options):
+    if name == "__init__":
+        return False
+    return would_skip
+
+def setup(app):
+    app.connect("autodoc-skip-member", skip)
+
 extensions = ['sphinx.ext.autodoc']
 
 html_css_files = ["https://cs50.readthedocs.io/_static/custom.css?" + str(round(time.time()))]
