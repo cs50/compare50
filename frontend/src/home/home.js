5a80c6fe11608c1997137b4d432de35ffbcf5f99
Joshua Archibald <jarchibald121@gmail.com>
2020-07-29 13:21:41 -0400

    Cleaning up the demo

--- frontend/src/home/home.js
+++ frontend/src/home/home.js
@@ -5,15 +5,15 @@ import Graph from './graph'
 
 function HomeView() {
     return (
-      <div>
         <div>
-          <Logo />
-        </div>
-        <div>
-          <Graph
-            graph='{"data": {"../compare50-cs50/compare50/_renderer/static": {"is_archive": false}, "compare50/_renderer/static": {"is_archive": false}, "frontend/src/home": {"is_archive": false}}, "links": [{"index": 0, "source": "compare50/_renderer/static", "target": "../compare50-cs50/compare50/_renderer/static", "value": 10.0}, {"index": 1, "source": "frontend/src/home", "target": "compare50/_renderer/static", "value": 2.1115042573076015}, {"index": 2, "source": "frontend/src/home", "target": "../compare50-cs50/compare50/_renderer/static", "value": 2.1115042573076015}, {"index": 3, "source": "frontend/src/home", "target": "compare50/_renderer/static", "value": 1.1703741938222783}, {"index": 4, "source": "frontend/src/home", "target": "../compare50-cs50/compare50/_renderer/static", "value": 1.1703741938222783}, {"index": 5, "source": "frontend/src/home", "target": "frontend/src/home", "value": 0.25548248403004065}], "nodes": [{"id": "compare50/_renderer/static"}, {"id": "frontend/src/home"}, {"id": "../compare50-cs50/compare50/_renderer/static"}, {"id": "frontend/src/home"}]}'
-            height="200" />
-        </div>
+            <div>
+                <Logo />
+            </div>
+            <div>
+                <Graph
+                  graph='{"data": {"../compare50-cs50/compare50/_renderer/static": {"is_archive": false}, "compare50/_renderer/static": {"is_archive": false}, "frontend/src/home": {"is_archive": false}}, "links": [{"index": 0, "source": "compare50/_renderer/static", "target": "../compare50-cs50/compare50/_renderer/static", "value": 10.0}, {"index": 1, "source": "frontend/src/home", "target": "compare50/_renderer/static", "value": 2.1115042573076015}, {"index": 2, "source": "frontend/src/home", "target": "../compare50-cs50/compare50/_renderer/static", "value": 2.1115042573076015}, {"index": 3, "source": "frontend/src/home", "target": "compare50/_renderer/static", "value": 1.1703741938222783}, {"index": 4, "source": "frontend/src/home", "target": "../compare50-cs50/compare50/_renderer/static", "value": 1.1703741938222783}, {"index": 5, "source": "frontend/src/home", "target": "frontend/src/home", "value": 0.25548248403004065}], "nodes": [{"id": "compare50/_renderer/static"}, {"id": "frontend/src/home"}, {"id": "../compare50-cs50/compare50/_renderer/static"}, {"id": "frontend/src/home"}]}'
+                  height="200" />
+            </div>
       </div>
     );
 }
