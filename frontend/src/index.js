1dd56c3826be71a3834544070a480cf3860220d9
jelleas <jelle.van.assema@gmail.com>
2020-08-12 22:35:04 +0200

    set title and favicon

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Helmet} from "react-helmet";
 import * as serviceWorker from './serviceWorker';
 
 // https://blog.logrocket.com/multiple-entry-points-in-create-react-app-without-ejecting/
@@ -14,8 +15,13 @@ else {
     throw new Error(`Env var REACT_APP_BUILD_TARGET is not set to either 'home' or 'match'`);
 }
 
+// Source SVGs (and emoji's) as favicon: https://stackoverflow.com/a/62438464
 ReactDOM.render(
     <React.StrictMode>
+        <Helmet>
+            <title>compare50</title>
+            <link rel="icon" href="data:image/svg+xml,%3Csvg%20xmlns='http://www.w3.org/2000/svg'%20viewBox='0%200%2016%2016'%3E%3Ctext%20x='0'%20y='14'%3E📝%3C/text%3E%3C/svg%3E" type="image/svg+xml" />
+        </Helmet>
         <BuildTarget />
     </React.StrictMode>,
     document.getElementById("root")
