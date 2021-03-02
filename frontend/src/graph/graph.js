28e2dddaf70e4ed498ac81d6b29ba29231e66794
Jelleas <jelle.van.assema@gmail.com>
2021-03-02 17:54:31 +0100

    react hooks in graph.js

--- frontend/src/graph/graph.js
+++ frontend/src/graph/graph.js
@@ -1,65 +1,65 @@
-import React from 'react';
+import React, {useRef, useEffect} from 'react';
 import '../index.css';
 import './graph.css';
 
 import D3Graph from './graph-d3';
 
-class Graph extends React.Component {
-    constructor(props) {
-        super(props);
-        this.divRef = React.createRef();
-        this.graphRef = React.createRef();
-        this.sliderRef = React.createRef();
-        this.d3Graph = null;
-    }
 
-    componentDidMount() {
-        this.d3Graph = new D3Graph.D3Graph(this.graphRef.current, {
+function Graph(props) {
+    const divRef = useRef(null);
+    const graphRef = useRef(null);
+    const sliderRef = useRef(null);
+    const d3Graph = useRef(null);
+
+    // once this component has mounted, create the d3graph
+    useEffect(() => {
+        d3Graph.current = new D3Graph.D3Graph(graphRef.current, {
             radius: 10,
-            width: this.props.width,
-            height: this.props.height,
-            color: this.props.color,
-            callbacks: this.props.callbacks,
-            cutoff: this.props.cutoff
+            width: props.width,
+            height: props.height,
+            color: props.color,
+            callbacks: props.callbacks,
+            cutoff: props.cutoff
         });
 
-        if (this.props.slider) {
-            this.d3Graph.addSlider(this.sliderRef.current);
+        if (props.slider) {
+            d3Graph.current.addSlider(sliderRef.current);
         }
 
-        // Initialize the graph
-        this.d3Graph.load(this.props.graph);
+        d3Graph.current.load(props.graph);
 
-        window.addEventListener("resize", this.d3Graph.onResize.bind(this.d3Graph));
-    }
+        const resizeListener = d3Graph.current.onResize.bind(d3Graph.current);
+        window.addEventListener("resize", resizeListener);
 
-    componentDidUpdate() {
-        this.d3Graph.setHighlighted(this.props.highlighted);
-    }
+        // once this component unmounts, remove the listener and destroy the d3graph
+        return () => {
+            window.removeEventListener("resize", resizeListener);
+            d3Graph.current.destroy();
+        }
+    // This effect can only run once, deliberitely passing [] to never run it twice
+    // https://reactjs.org/docs/hooks-effect.html#tip-optimizing-performance-by-skipping-effects
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
-    componentWillUnmount() {
-        window.removeEventListener("resize", this.d3Graph.onResize.bind(this.d3Graph));
-        this.d3Graph.destroy();
-    }
+    // when this component updates, update the highlighted selection in the d3Graph
+    useEffect(() => d3Graph.current.setHighlighted(props.highlighted));
 
-    render() {
-        return (
-            <div ref={this.divRef} style={{"width": "100%", "height":"100%"}}>
-                <svg className="d3graph" ref={this.graphRef}></svg>
-                <div className="d3slider" ref={this.sliderRef}>
-                    {this.props.sliderTip &&
-                    <small
-                        className="tooltip-marker"
-                        style={{position: "absolute", marginTop: "20px"}}
-                        data-tip="Slide this to hide all matches below the selected score."
-                    >
-                        ?
-                    </small>
-                    }
-                </div>
+    return (
+        <div ref={divRef} style={{"width": "100%", "height":"100%"}}>
+            <svg className="d3graph" ref={graphRef}></svg>
+            <div className="d3slider" ref={sliderRef}>
+                {props.sliderTip &&
+                <small
+                    className="tooltip-marker"
+                    style={{position: "absolute", marginTop: "20px"}}
+                    data-tip="Slide this to hide all matches below the selected score."
+                >
+                    ?
+                </small>
+                }
             </div>
-        )
-    }
-};
+        </div>
+    )
+}
 
 export default Graph;
