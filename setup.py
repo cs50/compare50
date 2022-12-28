7290948b08e456136d48bc082e6688bb09ddc8b5
Rongxin Liu <rongxinliu.dev@gmail.com>
2022-12-28 18:52:52 -0500

    added long description

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ setup(
     ],
     license="GPLv3",
     description="This is compare50, with which you can compare files for similarities.",
+    long_description="This is compare50, with which you can compare files for similarities.",
     install_requires=["attrs>=18,<21", "intervaltree>=3,<4", "lib50>=2,<4", "numpy>=1.15,<2", "pygments>=2.2,<3", "jinja2>=3,<4", "termcolor>=1.1.0,<2", "tqdm>=4.32,<5"],
     extras_require = {
         "develop": ["sphinx", "sphinx_rtd_theme", "sphinx-autobuild", "line_profiler"]
